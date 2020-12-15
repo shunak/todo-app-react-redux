9fca67ab570e38404ae45bc657a91ebed9d86773
shunak <shuna966@gmail.com>
2020-12-15 14:54:04 +0900

    first commit

---
+++ src/features/task/TaskList.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import TaskItem from './TaskItem';
+import { useSelector } from 'react-redux';
+import { selectTasks } from "./taskSlice";
+
+// functional component
+const TaskList = () => {
+    const tasks = useSelector(selectTasks);
+    return (
+        <>
+            {tasks.map((task) => (
+                <TaskItem key={task.id} task={task}/>
+            ))}
+        </>
+   );
+}
+
+export default TaskList
---
+++ src/features/task/taskSlice.js
@@ -0,0 +1,43 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const taskSlice = createSlice({
+  name: 'task',
+  initialState: {
+      idCount: 3,
+      tasks: [
+          {id: 3, title: "TASK C", completed: false },
+          {id: 2, title: "TASK B", completed: true },
+          {id: 1, title: "TASK A", completed: false },
+      ],
+  },
+  reducers: {
+    newTask: (state, action) => {
+      // Redux Toolkit allows us to write "mutating" logic in reducers. It
+      // doesn't actually mutate the state because it uses the Immer library,
+      // which detects changes to a "draft state" and produces a brand new
+      // immutable state based off those changes
+      state.idCount ++;
+      const newItem = {
+          id: state.idCount,
+          title: action.payload,
+          completed: false,
+      }
+      state.tasks = [newItem, ...state.tasks]
+    },
+    completeTask: (state, action) => {
+        const task = state.tasks.find((t)=>t.id === action.payload.id)
+        if (task) {
+            task.completed = !task.completed;
+        }
+    },
+    deleteTask: (state, action) => {
+        state.tasks = state.tasks.filter((t)=>t.id !== action.payload.id)
+    },
+  },
+});
+
+export const { newTask, completeTask, deleteTask} = taskSlice.actions;
+
+export const selectTasks= state => state.task.tasks;
+
+export default taskSlice.reducer;
